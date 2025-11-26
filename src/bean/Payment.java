5bca36483205812d43d97c4b043c57b7539c3abe
k_sugimoto <k_sugimoto@LAPTOP-P1NTU61R>
2025-11-26 10:40:09 +0900

    sg

--- src/bean/Payment.java
+++ src/bean/Payment.java
@@ -1,19 +1,19 @@
 package bean;
 
 public class Payment {
-	private Int Payment_ID;
-	private Int Payment_Type;
+	private int Payment_ID;
+	private int Payment_Type;
 
 	public int getPayment_ID() {
 		return Payment_ID;
 	}
-	public void setCd(Int Payment_ID) {
+	public void setPayment_ID(int Payment_ID) {
 		this.Payment_ID = Payment_ID;
 	}
 	public int getPayment_Type() {
 		return Payment_Type;
 	}
-	public void setPayment_Type(Int Payment_Type) {
+	public void setPayment_Type(int Payment_Type) {
 		this.Payment_Type = Payment_Type;
 	}
 }
