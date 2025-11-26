0e7a64e600d386b5835455d7d6d85317ca003e44
k_sugimoto <k_sugimoto@LAPTOP-P1NTU61R>
2025-11-26 11:47:31 +0900

    sg

--- src/bean/Purchase.java
+++ src/bean/Purchase.java
@@ -2,14 +2,54 @@ package bean;
 
 public class Purchase {
 	private int Purchase_ID;
-	UserBean userID ;
-	Cart Cart_ID
+	private String userID;
+	private String Cart_ID;
 	private String Purchase_Datetime;
-	Total_Amount
-	Shipping_Address
-	Payment_ID
+	private int Total_Amount;
+	private String Shipping_Address;
+	private int Payment_ID;
 
+	public int getPurchase_ID() {
+		return Purchase_ID;
+	}
+	public void setPurchase_ID(int Purchase_ID) {
+		this.Purchase_ID = Purchase_ID;
+	}
 
+	public String getuserID() {
+		return userID;
+	}
+	public void setuserID(String userID) {
+		this.userID = userID;
+	}
+
+	public String getCart_ID() {
+		return Cart_ID;
+	}
+	public void set(String Cart_ID) {
+		this.Cart_ID = Cart_ID;
+	}
+
+	public String getPurchase_Datetime() {
+		return Purchase_Datetime;
+	}
+	public void setPurchase_Datetime(String Purchase_Datetime) {
+		this.Purchase_Datetime = Purchase_Datetime;
+	}
+
+	public int getTotal_Amount() {
+		return Total_Amount;
+	}
+	public void setTotal_Amount(int Total_Amount) {
+		this.Total_Amount = Total_Amount;
+	}
+
+	public String getShipping_Address() {
+		return Shipping_Address;
+	}
+	public void setShipping_Address(String Shipping_Address) {
+		this.Shipping_Address= Shipping_Address;
+	}
 
 	public int getPayment_ID() {
 		return Payment_ID;
@@ -17,10 +57,5 @@ public class Purchase {
 	public void setPayment_ID(int Payment_ID) {
 		this.Payment_ID = Payment_ID;
 	}
-	public int getPayment_Type() {
-		return Payment_Type;
-	}
-	public void setPayment_Type(int Payment_Type) {
-		this.Payment_Type = Payment_Type;
-	}
+
 }
