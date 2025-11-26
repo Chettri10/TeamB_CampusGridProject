53d3efe269abd7aa3edc95374e02c05adfd9de7a
k_sugimoto <k_sugimoto@LAPTOP-P1NTU61R>
2025-11-26 11:25:29 +0900

    sg

---
+++ src/bean/Purchase.java
@@ -0,0 +1,26 @@
+package bean;
+
+public class Purchase {
+	private int Purchase_ID;
+	UserBean userID ;
+	Cart Cart_ID
+	private String Purchase_Datetime;
+	Total_Amount
+	Shipping_Address
+	Payment_ID
+
+
+
+	public int getPayment_ID() {
+		return Payment_ID;
+	}
+	public void setPayment_ID(int Payment_ID) {
+		this.Payment_ID = Payment_ID;
+	}
+	public int getPayment_Type() {
+		return Payment_Type;
+	}
+	public void setPayment_Type(int Payment_Type) {
+		this.Payment_Type = Payment_Type;
+	}
+}
